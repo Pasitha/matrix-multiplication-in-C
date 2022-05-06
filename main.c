76039c60fb0da79fdfd33c9cfca917259385a0c9
Pasitha <pasithacppsom@hotmail.com>
2022-05-07 01:01:05 +0700

    add printMatrix Function and random matrix generating

--- main.c
+++ main.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <pthread.h>
 
 #define MATRIX_SIZE 3
 
+void printMatrix(int matrix[MATRIX_SIZE][MATRIX_SIZE]) {
+    for (int i = 0; i < MATRIX_SIZE; i++) {
+        for (int j = 0; j < MATRIX_SIZE; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 int main(int argc, char *argv[]) {
-    
+    srand(time(0));
+
+    // matrix A, B
+    int matrixA[MATRIX_SIZE][MATRIX_SIZE];
+    int matrixB[MATRIX_SIZE][MATRIX_SIZE];
+
+    // random value in matrix
+    for (int i = 0; i < MATRIX_SIZE; i++) {
+        for (int j = 0; j < MATRIX_SIZE; j++) {
+            matrixA[i][j] = rand() % 10;
+            matrixB[i][j] = rand() % 10;
+        }
+    }
+
+    printMatrix(matrixA);
+    printMatrix(matrixB);
     return 0;
 }
