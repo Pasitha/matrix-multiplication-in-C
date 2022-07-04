fdd5119663e0d6609c65dd915d55fdad20f9f424
Pasitha <pasithacppsom@hotmail.com>
2022-07-04 23:08:11 +0700

    create thread for each calculation

--- main.c
+++ main.c
@@ -30,20 +30,21 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    // multiplication
+    // Definition: 
+    //     the entry c_ij = sum from k=1 to n a_ik b_kj
+    //     when a is m x n matrix and b is n x p matrix c is matrix product
+
     /////////////////////////////////////////////////////////////////////////////////
     //                               single thread                                 //
     /////////////////////////////////////////////////////////////////////////////////
 
-
-    // multiplication
-    // Definition: 
-    //     c_ij = sum from k=1 to n a_ik b_kj ; when a is m x n matrix and b is n x p matrix c is matrix product
     int matrixC[MATRIX_SIZE][MATRIX_SIZE];
 
     for (int i = 0; i < MATRIX_SIZE; i++) {
         for (int j = 0; j < MATRIX_SIZE; j++) {
             int sum = 0;
-            // sum
+            // sum of multiplying term-by-term the entries
             for (int k = 0; k < MATRIX_SIZE; k++) {
                 sum += matrixA[i][k] * matrixB[k][j];
             }
@@ -67,11 +68,33 @@ int main(int argc, char *argv[]) {
     /////////////////////////////////////////////////////////////////////////////////
 
     // multiplication of matrix using thread
-    int count = 0;
-    int* data = NULL;
     pthread_t* threads;
+    //declaring array of threads which is 
     threads = (pthread_t*)malloc(MATRIX_SIZE * MATRIX_SIZE * sizeof(pthread_t));
 
+    int count = 0;
+    // use for storing row and column elements
+    int* data = NULL;
+
+    for (int row = 0; row < MATRIX_SIZE; row++) {
+        for (int col = 0; col < MATRIX_SIZE; col++) {
+            // storing row and column elements in data
+            data = (int *)malloc((20)*sizeof(int));
+            
+            // storing size of matrix
+            data[0] = MATRIX_SIZE;
+
+            for (int i = 0; i < MATRIX_SIZE; i++)
+                data[i + 1] = matrixA[row][i];
+
+            for (int i = 0; i < MATRIX_SIZE; i++)
+                data[i + MATRIX_SIZE + 1] = matrixB[i][col];
+
+            // create thread for calculate entries c_ij
+            pthread_create(&threads[count++], NULL, multi, (void*)(data));
+
+        }
+    }
 
 #ifdef DISPLAY_MATRIX
     
